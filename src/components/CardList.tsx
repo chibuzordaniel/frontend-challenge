eb8c568e54a7af821a85c3f754fb3315887c60b2
chibuzordaniel <starchrischibuzor@gmail.com>
2023-03-05 06:19:09 +0100

    update file

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 import img1 from  '../assests/Rectangle 143.png'
 import img2 from  '../assests/Rectangle 149.png'
 import img3 from  '../assests/Rectangle 151.png'
-// import storeItems from "../data/item.json"
+import {Record} from '../data/Record';
+
 
 
 const CardList = () => {
@@ -20,93 +21,26 @@ const CardList = () => {
                                 </svg>
                                 <input className=' lg:pt-0 md:pt-0 sm:pt-3  outline-none' type="text"  placeholder='Search for laptops'/>
                             </div>
-                            <button className=' rounded-full bg-black  lg:w-40 lg:h-14 text-white font-semibold lg:mt-0 md:mt-0 lg:relative md:w-40 md:h-14 md:relative sm:absolute  sm:mt-7 sm:h-12 sm:w-[100%] '>Shop Now</button>
+                            <button className=' rounded-full bg-black  lg:w-40 lg:h-14 text-white font-semibold lg:mt-0 md:mt-0 lg:relative md:w-40 md:h-14 md:relative sm:absolute  sm:mt-7 sm:h-12 sm:w-[100%] '>Search Now</button>
                         </div>
                     </div>
                 </div>
                 <div className=' lg:grid grid-cols-4 lg:mt-40 md:mt-40 sm:mt-56 gap-12 md:block '>
 
-                    {/* <div>
-                      {storeItems.map(item => (
-                        <div key={item.id}>{JSON.stringify(item)}</div>
-                        
-
-                      ))}
-                    </div> */}
-                    
-
-                    <div className=' lg:bg-white lg:w-[261px] lg:h-[295px] lg:pb-0 rounded-xl shadow md:w-[90%] md:h-[100%] md:pb-12 ml-8 lg:ml-0  md:mt-20 sm:w-[80%] sm:pb-6 '>
-                        <img className=' md:w-[100%]  sm:w-[100%] ' src={img2} alt="" />
-                        <div className=' flex justify-between pt-2'>
-                            <h3 className=' pl-4  lg:text-sm font-bold md:text-3xl'>Macbook Air</h3>
-                            <p className='text-[#8A0B0B] lg:text-sm pr-4 font-bold  md:text-3xl'>N25,000</p>
-                        </div>
-                        <button className=' bg-[#8A0B0B] lg:w-20 lg:h-8 lg:text-sm text-white ml-4 lg:mt-1 font-semibold md:w-44 md:h-14 md:mt-5 md:text-xl sm:w-[32%] sm:h-8'>Add Cart</button>
-                    </div>
-
-                    <div className=' lg:bg-white lg:w-[261px] lg:h-[295px] lg:pb-0 rounded-xl shadow md:w-[90%] md:h-[100%] md:pb-12 ml-8  lg:ml-0  md:mt-20 sm:w-[80%] sm:mt-10 sm:pb-6'>
-                        <img className=' md:w-[100%]  sm:w-[100%]  ' src={img3} alt="" />
-                        <div className=' flex justify-between pt-2'>
-                            <h3 className=' pl-4  lg:text-sm font-bold md:text-3xl'>Macbook Air</h3>
-                            <p className='text-[#8A0B0B] lg:text-sm pr-4 font-bold  md:text-3xl'>N25,000</p>
-                        </div>
-                        <button className=' bg-[#8A0B0B] lg:w-20 lg:h-8 lg:text-sm text-white ml-4 lg:mt-1 font-semibold md:w-44 md:h-14 md:mt-5 md:text-xl sm:w-[32%] sm:h-8'>Add Cart</button>
-                    </div>
-
-                    <div className=' lg:bg-white lg:w-[261px] lg:h-[295px] lg:pb-0 rounded-xl shadow md:w-[90%] md:h-[100%] md:pb-12 ml-8  lg:ml-0  md:mt-20 sm:w-[80%] sm:mt-10 sm:pb-6 '>
-                        <img className=' md:w-[100%]  sm:w-[100%]  ' src={img1} alt="" />
-                        <div className=' flex justify-between pt-2'>
-                            <h3 className=' pl-4  lg:text-sm font-bold md:text-3xl'>Macbook Air</h3>
-                            <p className='text-[#8A0B0B] lg:text-sm pr-4 font-bold  md:text-3xl'>N25,000</p>
-                        </div>
-                        <button className=' bg-[#8A0B0B] lg:w-20 lg:h-8 lg:text-sm text-white ml-4 lg:mt-1 font-semibold md:w-44 md:h-14 md:mt-5 md:text-xl sm:w-[32%] sm:h-8'>Add Cart</button>
-                    </div>
-
-                    <div className=' lg:bg-white lg:w-[261px] lg:h-[295px] lg:pb-0 rounded-xl shadow md:w-[90%] md:h-[100%] md:pb-12 ml-8  lg:ml-0  md:mt-20 sm:w-[80%] sm:mt-10 sm:pb-6'>
-                        <img className=' md:w-[100%]   sm:w-[100%] ' src={img2} alt="" />
-                        <div className=' flex justify-between pt-2'>
-                            <h3 className='pl-4  lg:text-sm font-bold md:text-3xl'>Macbook Air</h3>
-                            <p className='text-[#8A0B0B] lg:text-sm pr-4 font-bold  md:text-3xl'>N25,000</p>
-                        </div>
-                        <button className=' bg-[#8A0B0B] lg:w-20 lg:h-8 lg:text-sm text-white ml-4 lg:mt-1 font-semibold md:w-44 md:h-14 md:mt-5 md:text-xl sm:w-[32%] sm:h-8'>Add Cart</button>
-                    </div>
-
-                    <div className=' lg:bg-white lg:w-[261px] lg:h-[295px] lg:pb-0 rounded-xl shadow md:w-[90%] md:h-[100%] md:pb-12 ml-8 lg:ml-0  md:mt-20 sm:w-[80%] sm:mt-10 sm:pb-6 '>
-                        <img className=' md:w-[100%]  sm:w-[100%]  ' src={img3} alt="" />
-                        <div className=' flex justify-between pt-2'>
-                            <h3  className=' pl-4  lg:text-sm font-bold md:text-3xl'>Macbook Air</h3>
-                            <p className='text-[#8A0B0B] lg:text-sm pr-4 font-bold  md:text-3xl'>N25,000</p>
-                        </div>
-                        <button className='  bg-[#8A0B0B] lg:w-20 lg:h-8 lg:text-sm text-white ml-4 lg:mt-1 font-semibold md:w-44 md:h-14 md:mt-5 md:text-xl sm:w-[32%] sm:h-8'>Add Cart</button>
-                    </div>
-
-                    <div className=' lg:bg-white lg:w-[261px] lg:h-[295px] lg:pb-0 rounded-xl shadow md:w-[90%] md:h-[100%] md:pb-12 ml-8  lg:ml-0  md:mt-20  sm:w-[80%] sm:mt-10 sm:pb-6'>
-                        <img className=' md:w-[100%]   sm:w-[100%] ' src={img1} alt="" />
-                        <div className=' flex justify-between pt-2'>
-                            <h3 className=' pl-4  lg:text-sm font-bold md:text-3xl'>Macbook Air</h3>
-                            <p className='text-[#8A0B0B] lg:text-sm pr-4 font-bold  md:text-3xl'>N25,000</p>
-                        </div>
-                        <button className='  bg-[#8A0B0B] lg:w-20 lg:h-8 lg:text-sm text-white ml-4 lg:mt-1 font-semibold md:w-44 md:h-14 md:mt-5 md:text-xl sm:w-[32%] sm:h-8'>Add Cart</button>
-                    </div>
-
-                    <div className=' lg:bg-white lg:w-[261px] lg:h-[295px] lg:pb-0 rounded-xl shadow md:w-[90%] md:h-[100%] md:pb-12 ml-8  lg:ml-0  md:mt-20  sm:w-[80%] sm:mt-10 sm:pb-6 '>
-                        <img className=' md:w-[100%]  sm:w-[100%]  ' src={img2} alt="" />
-                        <div className=' flex justify-between pt-2'>
-                            <h3 className=' pl-4  lg:text-sm font-bold md:text-3xl'>Macbook Air</h3>
-                            <p className='text-[#8A0B0B] lg:text-sm pr-4 font-bold  md:text-3xl'>N25,000</p>
-                        </div>
-                        <button className='  bg-[#8A0B0B] lg:w-20 lg:h-8 lg:text-sm text-white ml-4 lg:mt-1 font-semibold md:w-44 md:h-14 md:mt-5 md:text-xl sm:w-[32%] sm:h-8'>Add Cart</button>
-                    </div>
-
-                    <div className=' lg:bg-white lg:w-[261px] lg:h-[295px] lg:pb-0 rounded-xl shadow md:w-[90%] md:h-[100%] md:pb-12 ml-8  lg:ml-0  md:mt-20  sm:w-[80%] sm:mt-10 sm:pb-6 '>
-                        <img className=' md:w-[100%]  sm:w-[100%] ' src={img3} alt="" />
-                        <div className=' flex justify-between pt-2'>
-                            <h3 className='pl-4  lg:text-sm font-bold md:text-3xl'>Macbook Air</h3>
-                            <p className='text-[#8A0B0B] lg:text-sm pr-4 font-bold  md:text-3xl'>N25,000</p>
-                        </div>
-                        <button className='  bg-[#8A0B0B] lg:w-20 lg:h-8 lg:text-sm text-white ml-4 lg:mt-1 font-semibold md:w-44 md:h-14 md:mt-5 md:text-xl sm:w-[32%] sm:h-8'>Add Cart</button>
-                    </div>
-
+                    {
+                        Record.map((card, index) => (
+                            <div key={card.id} >
+                                <div className=' lg:bg-white lg:w-[261px] lg:h-[295px] lg:pb-0 rounded-xl shadow md:w-[90%] md:h-[100%] md:pb-12 ml-8 lg:ml-0  md:mt-20 sm:w-[80%] sm:pb-6 '>
+                                    <img className=' md:w-[100%]  sm:w-[100%] ' src={card.image} alt="" />
+                                    <div className=' flex justify-between pt-2'>
+                                      <h3 className=' pl-4  lg:text-sm font-bold md:text-3xl'> {card.name}</h3>
+                                       <p className='text-[#8A0B0B] lg:text-sm pr-4 font-bold  md:text-3xl'>{card.price}</p>
+                                    </div>
+                                    <button className=' bg-[#8A0B0B] lg:w-20 lg:h-8 lg:text-sm text-white ml-4 lg:mt-2 font-semibold md:w-44 md:h-14 md:mt-5 md:text-xl sm:w-[32%] sm:h-8'>Add Cart</button>
+                                </div>
+                            </div>
+                        ))
+                    }
                 </div>
             </div>
         </div>
