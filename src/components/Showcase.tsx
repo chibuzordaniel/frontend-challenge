4885ef35ea7b9bb817dc1194d3368add6704949d
chibuzordaniel <starchrischibuzor@gmail.com>
2023-03-08 15:16:09 +0100

    update file

--- src/components/Showcase.tsx
+++ src/components/Showcase.tsx
@@ -8,7 +8,7 @@ const Showcase = () => {
             <div className=' max-w-[1200px] mx-auto'>
                 <div className='lg:flex justify-between md:block  '>
                    <div className=' lg:mt-52 lg:ml-0 md:mt-52 md:ml-8 sm:mt-32 '>
-                     <h1 className=' lg:text-6xl lg:pl-0  md:text-6xl md:pl-0 sm:text-3xl  sm:pl-5'>Buy <span className=' font-bold'>Authentic</span> Laptops <br />  without worrying</h1>  
+                     <h1 className=' lg:text-6xl lg:pl-0  md:text-6xl md:pl-0 sm:text-3xl  sm:pl-5'>Buy <span className=' font-bold'>Authentic</span> Laptops <br />without worrying</h1>  
                      <button className=' bg-black lg:text-sm lg:w-40 lg:h-11 text-white lg:mt-20 rounded-full md:mt-20 md:ml-0 md:w-60 md:h-16 md:text-xl font-semibold sm:ml-5 sm:w-44 sm:h-10 mt-5'>Shop Now</button>
                    </div>
 
--- src/components/StoreItem.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const StoreItem = () => {
-  return (
-    <div>StoreItem</div>
-  )
-}
-
-export default StoreItem
