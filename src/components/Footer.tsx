c4ef0dfc3e9cd1595cbad99c088c57a042d2bf17
chibuzordaniel <starchrischibuzor@gmail.com>
2023-03-30 21:45:32 +0100

    update file

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
         <div className=' bg-[#EEEEEE] w-[100%] h-[100%] mt-40  pb-10'>
             <div className='border-[#dcd9d9] border-b-[1px] '>
                 <div className=' max-w-[1200px] mx-auto'>
-                    <div  className=' pl-5'>
+                    <div  className=''>
                         <div className=' lg:flex  justify-between pt-7 pb-4 md:block '>
                             <h1 className=' lg:text-lg  font-bold md:text-2xl '>FRONTEND CHALLENGE</h1>
                             <div className=' lg:flex lg:text-sm font-medium lg:pt-0 gap-12 md:block md:pt-10 md:text-lg sm:text-sm  sm:pt-4'>
@@ -21,7 +21,7 @@ const Footer = () => {
             </div>
 
             <div className=' max-w-[1200px] justify-between mx-auto pt-6'>
-                <div className=' lg:flex lg:text-sm justify-between md:block pl-5 md:text-lg'>
+                <div className=' lg:flex lg:text-sm justify-between md:block md:text-lg'>
                     <h2>@2019 wiz sullivan inc. All Rights Reserved</h2>
                    <div className=' flex gap-14 lg:pt-0 md:pt-4'>
                       <h3>Terms of Service</h3>
